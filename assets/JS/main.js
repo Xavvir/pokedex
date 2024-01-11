0176de7a07b9ae9d80c8b999f318fc3924015879
Xavvir <leandroxm1997@hotmail.com>
2024-01-10 23:26:39 -0300

    adicionando funcao ao Load More

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,45 +1,85 @@
 function convertPokemonTypesToli(pokemonTypes) {
-  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
-function convertPokemonToli(pokemon) {
-  return `
-    <li class="pokemon ${pokemon.type}">
-    <div class="dados">
-      <span class="number">#00${pokemon.number}</span>
-      <span class="name">${pokemon.name}</span>
-    </div>
-    <div class="datail">
-      <ol class="types">
-          ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-      </ol>
-      <picture>
-        <img
-          src="${pokemon.photo}"
-          alt="${pokemon.name}"
-        />
-      </picture>
-    </div>
-  </li>`;
+  return pokemonTypes.map(
+    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
+  );
 }
 
 const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 5;
+let offset = 0;
 
-pokeApi
-  .getPokemons()
-  .then((pokemons = []) => {
-    //Possibilidade 2
+// function convertPokemonToli(pokemon) {
+// return `
+//     <li class="pokemon ${pokemon.type}">
+//     <div class="dados">
+//       <span class="number">#00${pokemon.number}</span>
+//       <span class="name">${pokemon.name}</span>
+//     </div>
+//     <div class="datail">
+//       <ol class="types">
+//           ${pokemon.types
+//             .map((type) => `<li class="type ${type}">${type}</li>`)
+//             .join("")}
+//       </ol>
+//       <picture>
+//         <img
+//           src="${pokemon.photo}"
+//           alt="${pokemon.name}"
+//         />
+//       </picture>
+//     </div>
+//   </li>`;
+// }
 
-    pokemonList.innerHTML = pokemons.map(convertPokemonToli).join("");
-
-    //Possibilidade 1
-    // const listItens = [];
-
-    //   for (let i = 1; i < pokemons.length; i++) {
-    //     const pokemon = pokemons[i];
-    //     listItens.push(convertPokemonToli(pokemon));
-    //   }
-
-    //   console.log(listItens);
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) => `
+       <li class="pokemon ${pokemon.type}">
+          <div class="dados">
+            <span class="number">#00${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+          </div>
+        <div class="datail">
+          <ol class="types">
+              ${pokemon.types
+              .map((type) => `<li class="type ${type}">${type}</li>`)
+              .join("")}
+          </ol>
+        <picture>
+          <img
+            src="${pokemon.photo}"
+            alt="${pokemon.name}"
+          />
+        </picture>
+        </div>
+        </li>`).join('')
+    pokemonList.innerHTML += newHtml;
   })
-  .catch((error) => console.error(error));
+};
+
+loadPokemonItens(offset,limit);
+
+loadMoreButton.addEventListener('click', () => {
+  offset += limit
+  loadPokemonItens(offset, limit);
+})
+
+// pokeApi
+//   .getPokemons()
+//   .then((pokemons = []) => {
+//     //Possibilidade 2
+
+//     pokemonList.innerHTML = pokemons.map(convertPokemonToli).join("");
+
+//     //Possibilidade 1
+//     // const listItens = [];
+
+//     //   for (let i = 1; i < pokemons.length; i++) {
+//     //     const pokemon = pokemons[i];
+//     //     listItens.push(convertPokemonToli(pokemon));
+//     //   }
+
+//     //   console.log(listItens);
+//   })
+//   .catch((error) => console.error(error));
